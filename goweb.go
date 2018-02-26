97ce5a9c3371339a999991f0e5e3b31296588923
saurabhs051 <saurabhs051@gmail.com>
2018-02-26 23:54:24 +0530

    building a simple web server with Go

--- goweb.go
+++ goweb.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 ) //library for networking
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Go is neat")
+	fmt.Fprintf(w, "Whoa, Go is neat")
 }
 
-// func about_handler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Expert web design by Saurabh")
-// }
+func about_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Expert web design by Saurabh")
+}
 
 func main() {
 	http.HandleFunc("/", index_handler) //http handler //index_handler is a func
-	// http.HandleFunc("/about/", about_handler)
-	http.ListenAndServe(":8000", nil) //localhost port 8000
+	http.HandleFunc("/about/", about_handler)
+	err := http.ListenAndServe(":8000", nil) //localhost port 8000
+	if err != nil {
+		log.Fatal("ListenAndServe : ", err)
+	}
 
 }
